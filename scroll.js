acd5eab547becbadeaef350fbae95c8e2f320f24
Valter Sales <vsalesjr2008@gmail.com>
2024-10-26 22:16:14 -0300

    Terminando o header e o começo

---
+++ scroll.js
@@ -0,0 +1,6 @@
+document.getElementById("not1000").addEventListener("click", function() {
+    document.getElementById("noticias").scrollIntoView({
+        behavior: "smooth",
+        block: "end"
+    });
+});
